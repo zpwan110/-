a8bdaa288a590db8e3f0c12c20c1cc0027cb8af6
hzzp <2528478041@qq.com>
2018-11-29 23:48:59 +0800

    update .gitignore

---
+++ app/build/generated/source/buildConfig/app_H5_market/debug/com/crowd/curtain/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.crowd.curtain;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.crowd.curtain";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "app_H5_market";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.1";
+}
